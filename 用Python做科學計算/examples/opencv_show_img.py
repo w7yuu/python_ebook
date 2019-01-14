88be92bae65b4765ac9bb9872673826962d7f6df
testaccount <43800698+w7yuu@users.noreply.github.com>
2019-01-14 15:42:19 +0800

    Simple Chinese

--- 用Python做科學計算/examples/opencv_show_img.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from opencv.highgui import *
-import sys
-
-img = cvLoadImage( sys.argv[1] )
-cvNamedWindow("Example1", CV_WINDOW_AUTOSIZE)
-cvShowImage("Example1", img)
-cvWaitKey(0)
--- 用Python做科學計算/examples/tmp.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import matplotlib.pyplot as plt
-plt.subplot(221)
-plt.subplot(222)
-plt.subplot(212)
-plt.show()
